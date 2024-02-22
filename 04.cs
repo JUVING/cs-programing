ca989fc6603ae3339a78c5398a5a4d56e793744a
JUVING <129962308+JUVING@users.noreply.github.com>
2024-02-23 00:38:56 +0900

    Add files via upload

---
+++ 04.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*72pg
+class RelationalOperatorApp
+{
+    public static void Main()
+    {
+        int x = 3; int y = 5; int z = 7;
+        bool b1, b2;
+        b1 = x > y;
+        b2 = x >= y && y >= z;
+        Console.WriteLine("b1 = " + b1 + " b2 = " + b2);
+        b1 = x <= y;
+        b2 = x >= y == y >= x;
+        Console.WriteLine("b1 = " + b1 + " b2 = " + b2);
+    }
+}
+class LongicalAndApp
+{
+    public static void Main()
+    {
+        Console.WriteLine("TRUE AND TRUE = " + (true && true));
+        Console.WriteLine("TRUE AND FALSE = " + (true && false));
+        Console.WriteLine("FALSE AND TRUE = " + (false && true));
+        Console.WriteLine("FALSE AND FALSE = " + (false && false));
+    }
+}*/
+
+
+/* 81pg
+class ConditionalOperatorApp
+{
+    public static void Main()
+    {
+        int a,b,c;
+        int m;
+        Console.Write("Enter three numbers : ");
+        a = Console.Read() - '0';
+        b = Console.Read() - '0';
+        c = Console.Read() - '0';
+        m = (a > b) ? a : b;
+        m = (m > c) ? m : c;
+        Console.WriteLine("The lagest number : " + m);
+    }
+}
+class printTenItemApp
+{
+    public static void Main()
+    {
+        int i, n = 25;
+        for(i=1;i<=n;++i)
+            Console.Write(i + ((i % 10 == 0 | i == n) ? "\n" : ", "));
+    }
+}*/
+
+
+/*79pg
+class BitOperatorApp
+{
+    public static void Main()
+    {
+        int x = 9, y = 3;
+        Console.WriteLine(x + "&" + y + "=" + (x & y));
+        Console.WriteLine(x + "   " + y + " = " + (x | y));
+        Console.WriteLine(x + "^" + y + " = " + (x ^ y));
+        Console.WriteLine("~10 = " + (~10));
+    }
+}
+class ShiftPeratorApp
+{
+    public static void Main()
+    {
+        Console.WriteLine(" 10<<2= " + (10 << 2));
+        Console.WriteLine(" 10>>2= " + (10 >> 2));
+    }
+}*/
---
+++ 05.cs
@@ -0,0 +1,135 @@
+﻿using System;
+
+/*132pg
+class BreakstApp
+{
+    public static void Main()
+    {
+        int n, i;
+        double h = 0.0;
+        Console.Write("Enter a number = ");
+        n = Console.Read() - '0';
+        i = 1;
+        while(true)
+        {
+            h = h+1/(double) i;
+            if (++i > n) break;
+        }
+        Console.WriteLine(" n = " + n + ", h = " + h);
+    }
+}*/
+
+
+/*123pg
+class ForstApp
+{
+    public static void Main()
+    {
+        int i, n;
+        double h = 0.0;
+        Console.Write("Enter a number = ");
+        n = Console.Read() - '0';
+        for (i = 1; i <= n; ++i)
+            h = h + 1 / (double)i;
+        Console.WriteLine("n={0}, h={1}", n, h);
+    }
+}*/
+
+/*168pg
+class CallbyValueApp
+{
+    static void Swap(int x,int y)
+    {
+        int temp = x;x = y;y = temp;
+        Console.WriteLine("SWAP = x : {0}, y : {1}", x, y);
+    }
+    public static void Main()
+    {
+        int x = 1;int y = 2;
+        Console.WriteLine("befor = x : {0}, y : {1}", x, y);
+        Swap(x, y);
+        Console.WriteLine("after = x : {0}, y : {1}", x, y);
+    }
+}
+class CallbyReferanceApp
+{
+    static void Swap(ref int x,ref int y)
+    {
+        int temp = x; x = y; y = temp;
+        Console.WriteLine("SWAP = x : {0}, y : {1}", x, y);
+    }
+    public static void Main()
+    {
+        int x = 1; int y = 2;
+        Console.WriteLine("befor = x : {0}, y : {1}", x, y);
+        Swap(ref x, ref y);
+        Console.WriteLine("after = x : {0}, y : {1}", x, y);
+    }
+}
+class Integer
+{
+    public int i;
+    public Integer(int i)
+    {
+        this.i = i;
+    }
+}
+class CallbyObjectReferanceApp
+{ 
+    static void Swap(Integer x, Integer y)
+    {
+        int temp = x.i; x.i = y.i; y.i = temp;
+        Console.WriteLine("SWAP = x : {0}, y : {1}", x.i, y.i);
+    }
+    public static void Main()
+    {
+        Integer x = new Integer(1);
+        Integer y = new Integer(2);
+        Console.WriteLine("befor = x : {0}, y : {1}", x.i, y.i);
+        Swap(x, y);
+        Console.WriteLine("after = x : {0}, y : {1}", x.i, y.i);
+    }
+}*/
+
+/*172pg
+class ParamerArrayApp
+{
+    static void ParameterArray(params object [] obj)
+    {
+        for (int i = 0; i < obj.Length; i++)
+            Console.WriteLine(obj[i]);
+    }
+    public static void Main()
+    {
+        ParameterArray(123, "HELLO", true, 'A');
+    }
+}*/
+
+/*174pg
+class MethodOverloadingApp
+{
+    void somthing()
+    {
+        Console.WriteLine("somthing() is called.");
+    }
+    void somthing(int i)
+    {
+        Console.WriteLine("somthing(int) is called.");
+    }
+    void somthing(int i, int j)
+    {
+        Console.WriteLine("somthing(int, int) is called.");
+    }
+    void somthing(double d)
+    {
+        Console.WriteLine("somthing(double) is called.");
+    }
+    public static void Main()
+    {
+        MethodOverloadingApp obj = new MethodOverloadingApp();
+        obj.somthing(10);
+        obj.somthing(3.14);
+        obj.somthing();
+        obj.somthing(100, 63);
+    }
+}*/

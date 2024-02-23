3a0003d63019d28318d30450087f4ba830b5ae5c
JUVING <129962308+JUVING@users.noreply.github.com>
2024-02-24 02:51:31 +0900

    Add files via upload

---
+++ 07.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+/*
+class cat
+{
+    public string name;
+    public string color;
+    public void Meow()
+    {
+        Console.WriteLine("{0} : 야옹",name);
+    }
+   void print(params object [] obj)
+    {
+        foreach (object s in obj)
+            Console.WriteLine(s);
+    }
+    public static void Main()
+    {
+        cat kitty = new cat();
+        kitty.name = "kitty";
+        kitty.color = "black";
+        kitty.Meow();
+        kitty.print(kitty.name, kitty.color);
+    }
+}*/
---
+++ 08.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+/* 추상 클래스
+ abstract class animal
+{
+    public  abstract void MakeSound();
+}
+class dog : animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("멍멍");
+    }
+}
+
+class cat : animal
+{
+    public override void MakeSound()
+    {
+        Console.WriteLine("야옹");
+    }
+}
+
+class Test
+{
+    public static void Main()
+    {
+        animal Dog = new dog();
+        animal Cat = new cat();
+
+        Dog.MakeSound();
+        Cat.MakeSound();
+    }
+}*/

a3bb527da560050897790463332d59e5fc010308
JUVING <129962308+JUVING@users.noreply.github.com>
2024-02-26 02:58:08 +0900

    Add files via upload

---
+++ 11.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections;
+
+/* 일반화 메소드
+class Exercise10_1
+{
+    void copyarray(int[] source, int[] target)
+    {
+        for (int i = 0; i < source.Length; i++)
+            target[i] = source[i];
+    }
+    void copyarray(string[] source, string[] target)
+    {
+        for (int i = 0; i < source.Length; i++)
+            target[i] = source[i];
+    }
+    //일반화 메소드 적용
+    void copyarray<T>(T[] source, T[] target)
+    {
+        for (int i = 0; i < source.Length; i++)
+            target[i] = source[i];
+    }
+
+    public void Main()
+    {
+        //일반화 메소드 호출하기
+        int[] source = { 1, 2, 3, 4, 5 };
+        int[] target = new int[source.Length];
+        //자료형을 명시하여 호출한다.
+        copyarray<int>(source, target);
+    }
+    
+}*/
+
+
+/* 일반화 클래스
+class Exercise10_2
+{
+    class Array_Int
+    {
+        private int[] array;
+        public int GetElement(int index)
+        { return array[index]; }
+    }
+    class Array_Double
+    {
+        private double[] array;
+        public double GetElement(int index)
+        { return array[index]; }
+    }
+    //일반화 클래스
+    class Array_Generic<T>
+    {
+        private T[] array;
+        public T GetElement(int index)
+        { return array[index]; }
+    }
+    
+    public void Main()
+    {
+        //객체 생성하기
+        Array_Generic<int> Intarr = new Array_Generic<int>();
+        Array_Generic<double> Doublearr = new Array_Generic<double>();
+    }
+}*/
+
+
+/*
+public class Exercise10_3
+{
+    public static void Main()
+    {
+        List<int> list = new List<int>();
+        for (int i = 0; i < 5; i++)
+            list.Add(i);
+
+        foreach (int element in list)
+            Console.Write("{0} ", element);
+
+        Console.WriteLine();
+
+        list.RemoveAt(2);
+    }
+}*/
---
+++ 12.cs
@@ -0,0 +1,98 @@
+﻿using System;
+
+
+/* 예외 처리
+class Exercise11_1
+{
+    public static void Main()
+    {
+        int[] arr = { 1, 2, 3 };
+        try
+        {
+            for(int i=0;i<5;i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+        catch(IndexOutOfRangeException e) //Exception을 사용해도 무관하다.
+        {
+            Console.WriteLine("예외가 발생했습니다. : {0} ",e.Message);
+        }
+        Console.WriteLine("종료");
+    }
+}
+
+//예외 일으키기
+
+class UserErrException : ApplicationException
+{
+    public UserErrException(string s) : base(s) { }
+}
+class UserException
+{
+    public static void Main()
+    {
+        try
+        {
+            throw new UserErrException("throw async exception with a message");
+        }
+        catch(UserErrException e)
+        {
+            Console.WriteLine(e.Message);
+        }
+    }
+}*/
+
+/* try, catch, throw, finally
+class Exercise12_2
+{
+    static void Dosomething(int arg)
+    {
+        if (arg < 10)
+            Console.WriteLine("arg : {0}", arg);
+        else
+            throw new Exception("arg가 10보다 큽니다.");
+    }
+    public static void Main()
+    {
+        try
+        {
+            Dosomething(13);
+        }
+        catch(Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+        finally
+        {
+            Console.WriteLine("프로그램 종료...");
+        }
+    }
+}*/
+
+/*
+class Exercise12_3
+{
+    class FilterableException : Exception
+    {
+        public int ERRORNO { get; set; }
+    }
+    public static void Main()
+    {
+        try
+        {
+            Console.Write(">>> ");
+            int num = int.Parse(Console.ReadLine());
+            
+            if (num < 0 || num > 10)
+                throw new FilterableException() { ERRORNO = num };
+            else
+                Console.WriteLine($"output : {num}");
+        }
+        catch(FilterableException e) when (e.ERRORNO<0)
+        {
+            Console.WriteLine("Negatice input is not allowed.");
+        }
+    }
+
+}*/
